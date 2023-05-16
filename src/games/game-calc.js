3f197922587a5d9242422afb0d66f8cac215e255
kos342 <kolesnikov.ks@gmail.com>
2023-05-16 18:10:30 +0200

    add game-calc.js

---
+++ src/games/game-calc.js
@@ -0,0 +1,57 @@
+import startGame from '../index/js'
+
+export default () => {
+	const gameTerms = 'What is the result of the expression?';
+	const getQuestion = () => {
+		const getOperator = () => {
+			const randomValue = Math.ceil(Math.random() * 3);
+			let operator = '';
+
+			switch (randomValue) {
+				case 1: {
+					operator = '+';
+					break;
+				}
+				case 2: {
+					operator = '-';
+					break;
+				}
+				case 3: {
+					operator = '*';
+					break;
+				}
+				default: 
+					break;
+			};
+			const number1 = Math.ceil(Math.random() * 100);
+			const number2 = Math.ceil(Math.random() * 100);
+			const operator = getOperator();
+			const expression = `${number1} ${operator} ${number2}`;
+
+			return expression;
+		};
+
+		const getCorrectAnswer = (value) => {
+			const array = value.split(' ');
+			const [number1, operator, number2] = array;
+			let correctAnswer = 0;
+
+			switch (operator) {
+				case '+': {
+					correctAnswer = +number1 + +number2;
+					break;
+				}
+				case '-': {
+					correctAnswer = +number1 - +number2;
+					break;
+				}
+				case '*': {
+					correctAnswer = +number1 * +number2;
+					break;
+				}
+				default:
+					break;
+			};
+
+			startGame(gameTerms, getQuestion, getCorrectAnswer);
+		};
