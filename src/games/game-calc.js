5c5585c0eab7cfd819010e6a0c79cabc6b76ffce
kos342 <kolesnikov.ks@gmail.com>
2023-05-16 22:09:28 +0200

    change game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,59 +1,37 @@
-import startGame from '../index.js'
+import gameEngine from '../index.js';
+import getRandomInRange from '../getRandomInRange.js';
+
+const gameDescription = 'What is the result of the expression?';
+
+const getRandomOperators = () => {
+  const operators = ['+', '-', '*'];
+  return operators[getRandomInRange(0, operators.length - 1)];
+};
+
+const calculation = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Operator ${operator} - doesn't supported);`);
+  }
+};
+
+const generateRound = () => {
+  const num1 = getRandomInRange(0, 10);
+  const num2 = getRandomInRange(0, 10);
+  const operator = getRandomOperators();
+
+  const question = String(`${num1} ${operator} ${num2}`);
+  const answer = String(calculation(num1, num2, operator));
+
+  return [question, answer];
+};
 
 export default () => {
-	const gameTerms = 'What is the result of the expression?';
-	const getQuestion = () => {
-		const getOperator = () => {
-			const randomValue = Math.ceil(Math.random() * 3);
-			let mathOperator = '';
-
-			switch (randomValue) {
-				case 1: {
-					mathOperator = '+';
-					break;
-				}
-				case 2: {
-					mathOperator = '-';
-					break;
-				}
-				case 3: {
-					mathOperator = '*';
-					break;
-				}
-				default: 
-					break;
-			};
-			const number1 = Math.ceil(Math.random() * 100);
-			const number2 = Math.ceil(Math.random() * 100);
-			const operator = getOperator();
-			const expression = `${number1} ${operator} ${number2}`;
-
-			return expression;
-		};
-
-		const getCorrectAnswer = (value) => {
-			const array = value.split(' ');
-			const [number1, operator, number2] = array;
-			let correctAnswer = 0;
-
-			switch (operator) {
-				case '+': {
-					correctAnswer = +number1 + +number2;
-					break;
-				}
-				case '-': {
-					correctAnswer = +number1 - +number2;
-					break;
-				}
-				case '*': {
-					correctAnswer = +number1 * +number2;
-					break;
-				}
-				default:
-					break;
-			};
-
-			startGame(gameTerms, getQuestion, getCorrectAnswer);
-		};
-	}
-}
+  gameEngine(gameDescription, generateRound);
+};
