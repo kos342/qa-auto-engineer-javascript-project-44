3070d4379082b499ad5486331d72e84de4cf5c4d
kos342 <kolesnikov.ks@gmail.com>
2023-05-17 15:38:10 +0200

    change gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,24 @@
+import gameEngine from "../index.js";
+import getRandomInRange from "../getRandomInRange.js";
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
+  } return getGcd(num2 % num1, num1);
+};
+
+const generateRound = () => {
+  const num1 = getRandomInRange(0, 100);
+  const num2 = getRandomInRange(0, 100);
+
+  const question = String(`${num1} ${num2}`);
+  const answer = String(getGcd(num1, num2));
+
+  return [question, answer];
+};
+
+export default () => {
+  gameEngine(gameDescription, generateRound);
+};
